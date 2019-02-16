a3d85f1003697df92ab33a52d1a61bf10d07c77d
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-02-17 01:04:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,9 +150,9 @@ def checkPostback(output):
       a=requests.get("https://graph.facebook.com/"+id+"?fields=first_name,last_name,profile_pic&access_token="+ACCESS_TOKEN)
       data=a.json()
       name=data['first_name']
-      if output['entry'][0]['messaging'][0]['postback']['payload']=='debangshutable1':
-         welcome='Welcome! debangshutable1 I am AI-powered bot studmenu,I will help you practice problems while having fun! Get ready for some interactive learning! :D'
-         send_message(id,'a','a', welcome)
+      #if output['entry'][0]['messaging'][0]['postback']['payload']=='debangshutable1':
+      welcome='Welcome!'+str(output['entry'][0]['messaging'][0]['postback']['payload'])+' I am AI-powered bot studmenu,I will help you practice problems while having fun! Get ready for some interactive learning! :D'
+      send_message(id,'a','a', welcome)
         
       
 def checkCalculator(id,text):
