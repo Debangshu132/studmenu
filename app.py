03443cad16e83c5821dfe320bc8f32393a2ba17f
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-02-19 23:54:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def checkPostback(output):
       name=data['first_name']
       if output['entry'][0]['messaging'][0]['postback']['payload']=='StartMan':
        if output['entry'][0]['messaging'][0]['postback'].get('referral'):
-         welcome='Welcome!'+str(output['entry'][0]['messaging'][0]['referral']['ref'])+' I am AI-powered bot studmenu,I will help you practice problems while having fun! Get ready for some interactive learning! :D'
+         welcome='Welcome!'+str(output['entry'][0]['messaging'][0]['postback']['referral']['ref'])+' I am AI-powered bot studmenu,I will help you practice problems while having fun! Get ready for some interactive learning! :D'
        else:
         welcome="Welcome! please open the camera and long press to scan the qr code!"
        send_message(id,'a','a', welcome)
