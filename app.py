27667ce154726a4ca71fbcd297ada1eab24a5fef
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-02-19 22:50:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -153,6 +153,15 @@ def checkReferral(output):
       #if output['entry'][0]['messaging'][0]['postback']['payload']=='debangshutable1':
       welcome='Welcome!'+str(output['entry'][0]['messaging'][0]['referral']['ref'])+' I am AI-powered bot studmenu,I will help you practice problems while having fun! Get ready for some interactive learning! :D'
       send_message(id,'a','a', welcome)
+def checkPosrback(output):
+     if output['entry'][0]['messaging'][0].get('postback'):
+      id=  output['entry'][0]['messaging'][0]['sender']['id']  
+      a=requests.get("https://graph.facebook.com/"+id+"?fields=first_name,last_name,profile_pic&access_token="+ACCESS_TOKEN)
+      data=a.json()
+      name=data['first_name']
+      #if output['entry'][0]['messaging'][0]['postback']['payload']=='debangshutable1':
+      welcome='Welcome!'+str(output['entry'][0]['messaging'][0]['postback']['referral']['ref'])+' I am AI-powered bot studmenu,I will help you practice problems while having fun! Get ready for some interactive learning! :D'
+      send_message(id,'a','a', welcome)    
 
     
         
