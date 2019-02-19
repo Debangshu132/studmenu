dfe3373b6a3c1b5bf97fc00d6dd4b5deba402336
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-02-19 22:36:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def checkPostback(output):
       name=data['first_name']
       #if output['entry'][0]['messaging'][0]['postback']['payload']=='debangshutable1':
       welcome='Welcome!'+str(output['entry'][0]['messaging'][0]['referral']['ref'])+' I am AI-powered bot studmenu,I will help you practice problems while having fun! Get ready for some interactive learning! :D'
-      #send_message(id,'a','a', welcome)
+      send_message(id,'a','a', welcome)
 
     
         
