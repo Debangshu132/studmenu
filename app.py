730c2e3ffbc25da46f1d6d2ca07e0086db64e02b
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-02-20 19:01:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -236,11 +236,11 @@ def send_message(recipient_id, topic,mood,response):
     return "success"
 
 def updateUsersInformation(ID, **kwargs):
-    MONGODB_URI = "mongodb://Debangshu:Starrynight.1@ds121461.mlab.com:21461/flunkey"
+    MONGODB_URI = "mongodb://Debangshu:Studmenu.1@ds121461.mlab.com:21461/flunkey"
     client = MongoClient(MONGODB_URI, connectTimeoutMS=30000)
-    db = client.get_database("flunkey2")
-    #for key in kwargs:
-    #    db.userInfo.update({"_id": {"$oid": "5c6d52d2fb6fc01c4ce866b2"}}, {"$set":{str(ID)+"."+str(key): kwargs[key]}},upsert=True)
+    db = client.get_database("flunkey")
+    for key in kwargs:
+        db.userInfo.update({"_id": "5c6d52d2fb6fc01c4ce866b2"}, {"$set":{str(ID)+"."+str(key): kwargs[key]}},upsert=True)
     return(0)
 def getUserInformation(id,property):
     MONGODB_URI = "mongodb://Debangshu:Starrynight.1@ds121461.mlab.com:21461/flunkey"
