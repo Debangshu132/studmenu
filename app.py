29c073f1dd909fd5286643817500bf5a55bfd91c
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-02-20 16:12:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -159,8 +159,11 @@ def checkReferral(output):
       instruction="To open menu press Open Menu, To call the waiter press Call Waiter"
       send_message(id,'a','a', welcome)
       #quickreply(id,['Open Menu','Call Waiter'],instruction)
-      button= [{ "type": "web_url","url": "https://www.google.com/", "title": "Open Menu" }] 
-      bot.send_button_message(id,'To open menu press Open Menu ',button)   
+      button= [{ "type": "web_url","url": "https://www.google.com/", "title": "Open Menu" },
+              {"type":"postback","title":"CALL WAITER","payload":"waiter"}] 
+      bot.send_button_message(id,'To open menu press Open Menu ',button)  
+     if output['entry'][0]['messaging'][0]['postback']['payload']=='waiter':
+        quickreply(id,['Napkins','Spoons',"Water","Talk to waiter"],"Calling waiter what do you want?")   
 def checkPostback(output):
      if output['entry'][0]['messaging'][0].get('postback'):
       id=  output['entry'][0]['messaging'][0]['sender']['id']  
@@ -180,8 +183,11 @@ def checkPostback(output):
        instruction="To open menu press Open Menu, To call the waiter press Call Waiter"
        #initializeUser(id) 
        #quickreply(id,['Open Menu','Call Waiter'],instruction)
-       button= [{ "type": "web_url","url": "https://www.google.com/", "title": "Open Menu" }] 
+       button= [{ "type": "web_url","url": "https://www.google.com/", "title": "Open Menu" },
+               {"type":"postback","title":"CALL WAITER","payload":"waiter"}] 
        bot.send_button_message(id,'To open menu press Open Menu ',button) 
+      if output['entry'][0]['messaging'][0]['postback']['payload']=='waiter':
+        quickreply(id,['Napkins','Spoons',"Water","Talk to waiter"],"Calling waiter what do you want?")
         
       
 def checkCalculator(id,text):
