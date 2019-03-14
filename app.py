d24b3bebe1e70f58de64ffd635944c19b2107539
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-14 18:01:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ def executeConsumerCode(id,fulladdress,name,restaurant,tableno):
        welcome='Welcome!'+name+" you are sitting in restaurant "+restaurant+" in table number "+ tableno+" I am your host today :)"
        send_message(id,'a','a', welcome)
        instruction="To open menu press Open Menu, To call the waiter press Call Waiter"
-       button= [{ "type": "web_url","url": "https://www.google.com",
+       button= [{ "type": "web_url","url": "https://www.studmenuweb.herokuapp.com",
                  "title": "Menu","messenger_extensions":True },
                {"type":"postback","title":"Waiter","payload":"waiter"}] 
          
