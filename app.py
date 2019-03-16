e6ca7abb1e6dde3e81848fc5c3785b666a1d09cc
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-16 11:56:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from pymessenger.bot import Bot
 import os
 import requests
 import urllib
-
+from flask_cors import CORS
 import json
 from decisionTree import decision,listOfExams,askQuestion,handleResults,decisionRightWrong
 from intelligence import BRAIN
@@ -369,6 +369,7 @@ def initializeUser(id,category):
     if category=="consumer":
         updateConsumersInformation(id,name=name,name1="")  
 @app.route("/cart/<cartdata>", methods=['GET', 'POST'])
+@cross_origin()
 def cart(cartdata):
   if request.method == 'GET':
         """Before allowing people to message your bot, Facebook has implemented a verify token
