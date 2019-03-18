036f3b8b1624d6f94a802da679372bc1e6b1f5b4
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-18 16:10:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -424,7 +424,7 @@ def cart(cartdata):
     waiterid=table['waiter']
     send_message(consumer_id, "","","your order is placed!")
     send_message(waiterid, "","","Table number "+tableno+" has ordered!, the cart is: "+str(mycart))  
-    mycart={"cart":mycart,"profilepic":str(profilepic)}     
+    
     updateRestaurantsCartInformation(restaurant,tableno,**{consumer_id:mycart})   
     cartjson={"restaurant":restaurant,"tableno":tableno,"consumer_id":consumer_id}
     
