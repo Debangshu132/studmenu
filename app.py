3b9ff6ddb84b3172aaf23afad8d08d948b6c63f6
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-18 19:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ def cart(cartdata):
     send_message(consumer_id, "","","your order is placed!")
     send_message(waiterid, "","","Table number "+tableno+" has ordered!, the cart is: "+str(mycart))  
     
-    updateRestaurantsCartInformation(restaurant,tableno,**{consumer_id:json.dumps(mycart)})   
+    updateRestaurantsCartInformation(restaurant,tableno,**{consumer_id:json.dumps(cartdata)})   
     cartjson={"restaurant":restaurant,"tableno":tableno,"consumer_id":consumer_id}
     
     response=   {"recipient":{"id":consumer_id},"message":{"quick_replies": [
