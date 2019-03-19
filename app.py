e896ac5cac323e1dcc846e3400428b198ae78228
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-03-19 10:45:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -340,7 +340,7 @@ def updateRestaurantsCartInformation(nameOfRestaurant,tableno, **kwargs):
     cart=table["cart"]
     for key in kwargs:
       #cart[key]=kwargs[key]
-      db.restaurants.update({"_id" : "restaurant"}, {"$set":{str(nameOfRestaurant)+".tables."+str(tableno)+".cart."+str(key): kwargs[key]}},upsert=True);
+      db.restaurants.update({"_id" : "restaurant"}, {"$push":{str(nameOfRestaurant)+".tables."+str(tableno)+".cart."+str(key)+".mycart": kwargs[key]}},upsert=True);
     return(0)
 
 def updateRestaurantsTablesInformation(nameOfRestaurant,tableno, **kwargs):
