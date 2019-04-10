36dc5f86fac5e314062eaecf33a5a52bd3d98195
Debangshu Paul <34456060+Debangshu132@users.noreply.github.com>
2019-04-10 15:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -553,7 +553,7 @@ def cart(cartdata):
  
     
     print("yea")
-    print(cartdata)
+ """print(cartdata)
     consumer_id=json.loads(cartdata)["id"]
     mycart=json.loads(cartdata)["cart"]
 
@@ -568,11 +568,11 @@ def cart(cartdata):
     tables=getRestaurantsInformation(restaurant,"tables")
     table=tables[tableno]
     waiterid=table['waiter']
-    #updateRestaurantsTablesInformation(restaurant,tableno, whoLastOrdered=consumer_id)     
+    updateRestaurantsTablesInformation(restaurant,tableno, whoLastOrdered=consumer_id)     
     send_message(consumer_id, "","","Your order is pending!")
     #send_message(waiterid, "","","Table number "+tableno+" has ordered!, the cart is: "+str(mycart))  
-    #updateRestaurantsCartInformation(restaurant,tableno,**{consumer_id:{"firstname":firstname,"status":"pending","mycart":mycart}})   
-    #updateRestaurantsStatusInformation(restaurant,tableno,consumer_id, "pending","changeonlybucket")
+    updateRestaurantsCartInformation(restaurant,tableno,**{consumer_id:{"firstname":firstname,"status":"pending","mycart":mycart}})   
+    updateRestaurantsStatusInformation(restaurant,tableno,consumer_id, "pending","changeonlybucket")
     cartjsonconsumer={"restaurant":restaurant,"tableno":tableno,"identity":"consumer"}
     cartjsonwaiter={"restaurant":restaurant,"tableno":tableno,"identity":"waiter"}
     cartjsonmanager={"restaurant":restaurant,"tableno":tableno,"identity":"manager"}     
@@ -589,7 +589,7 @@ def cart(cartdata):
     button= [{ "type": "web_url","url": "https://studmenuweb.herokuapp.com/groupcart/"+json.dumps(cartjsonwaiter),
                  "title": "View Order","messenger_extensions": True}] 
     bot.send_button_message(waiterid,'Table: '+tableno,button) 
-    r=pay(responseconsumer) 
+    r=pay(responseconsumer) """
      
     return "success!"
 @app.route("/checkout/<data>", methods=['GET', 'POST'])
